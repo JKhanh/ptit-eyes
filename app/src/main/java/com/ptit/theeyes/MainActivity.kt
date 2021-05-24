21a8265c262779dcaeef33afb6c39dae3e802cbf
Nguyen Quoc Khanh <nguyenkhanhcv@gmail.com>
2021-05-24 16:58:32 +0700

    Add viewbinding delegate

--- app/src/main/java/com/ptit/theeyes/MainActivity.kt
+++ app/src/main/java/com/ptit/theeyes/MainActivity.kt
@@ -2,10 +2,11 @@ package com.ptit.theeyes
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.ptit.theeyes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by viewBinding(ActivityMainBinding::inflate)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
